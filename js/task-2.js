1761b96ae10d0f8dabf548960e47d74e2a539152
Oleksandr <alexmelovat@gmail.com>
2026-09-08 19:45:44 -0700

    add js.skript

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+function makeArray(firstArray, secondArray, maxLength) {
+  const combined = firstArray.concat(secondArray);
+
+  if (combined.length > maxLength) {
+    return combined.slice(0, maxLength);
+  }
+
+  return combined;
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
